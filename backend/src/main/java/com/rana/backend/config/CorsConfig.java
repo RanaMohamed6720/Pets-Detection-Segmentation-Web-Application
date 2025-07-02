3610e82fcdc461cd93df8d2aa0237197e0079b57
Rana Mohamed <121067602+RanaMohamed6720@users.noreply.github.com>
2025-07-02 20:04:08 +0300

    clean up cors file

--- backend/src/main/java/com/rana/backend/config/CorsConfig.java
+++ backend/src/main/java/com/rana/backend/config/CorsConfig.java
@@ -1,5 +1,7 @@
 package com.rana.backend.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -15,18 +17,16 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         // allow both local development and production domains
-        config.addAllowedOrigin("http://localhost:3000"); // local frontend
-        config.addAllowedOrigin("https://RanaMohamed6720.github.io"); // GitHub Pages
-        config.addAllowedOrigin("https://pets-detection-segmentation-web-application-production.up.railway.app");
+        config.setAllowedOriginPatterns(List.of(
+                "http://localhost:3000", // local frontend
+                "https://RanaMohamed6720.github.io", // GitHub Pages
+                "https://pets-detection-segmentation-web-application-production.up.railway.app"));
 
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("GET");
-        config.addAllowedMethod("POST");
-        config.addAllowedMethod("PUT");
-        config.addAllowedMethod("DELETE");
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
-
         source.registerCorsConfiguration("/**", config);
+
         return new CorsFilter(source);
     }
 }
